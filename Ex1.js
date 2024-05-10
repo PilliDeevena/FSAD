fe70903b33f7203b69266c665197d221c684f9fa
PilliDeevena <149320035+PilliDeevena@users.noreply.github.com>
2024-05-10 19:25:50 +0530

    Add files via upload

---
+++ Ex1.js
@@ -0,0 +1,13 @@
+// src/FunctionalComponent.js
+import React from 'react';
+
+const FunctionalComponent = () => {
+  return (
+    <div>
+      <h1>Functional Component</h1> 
+      <p>FSAD</p>
+    </div>
+  );
+};
+
+export default FunctionalComponent;
